d20066d754f1da72ab13059a2a3687485d0a0f73
Balazs Perlaki-Horvath <ba.perlaki@gmail.com>
2021-12-19 00:20:22 +0100

    Convert providers

--- app/src/main/java/com/irishinterest/irishinterest/fragments/detailedBooks/BookDetailedProvider.kt
+++ app/src/main/java/com/irishinterest/irishinterest/fragments/detailedBooks/BookDetailedProvider.kt
@@ -1,26 +1,24 @@
-package com.irishinterest.irishinterest.fragments.authors
+package com.irishinterest.irishinterest.fragments.detailedBooks
 
 import com.irishinterest.irishinterest.network.api.irishInterest.observer.GuiToModel
 import com.irishinterest.irishinterest.network.api.irishInterest.NotificationOnChange
-import com.irishinterest.irishinterest.model.authors.AuthorValues
+import com.irishinterest.irishinterest.model.books.BookValues
 import com.irishinterest.irishinterest.model.provider.Provider
 import com.irishinterest.irishinterest.network.api.irishInterest.observer.GuiMessage
 import com.irishinterest.irishinterest.network.api.irishInterest.observer.Module
 import java.util.*
-import kotlin.collections.ArrayList
 
-class AuthorsProvider(
+class BookDetailedProvider(
     guiToModel: GuiToModel,
-    notificationOnChange: NotificationOnChange<AuthorValues>,
-    override val data: ArrayList<AuthorValues> = ArrayList<AuthorValues>()
-) : Provider<AuthorValues>(
-    guiToModel, notificationOnChange
-), Observer {
-    protected override fun processMessage(guiMessage: GuiMessage<AuthorValues>?) {}
+    notificationOnChange: NotificationOnChange<BookValues>,
+    override val data: ArrayList<BookValues> = ArrayList<BookValues>()
+) : Provider<BookValues>(guiToModel, notificationOnChange), Observer {
+
+    protected override fun processMessage(guiMessage: GuiMessage<BookValues>?) {}
     override fun update(o: Observable, arg: Any) {}
 
     init {
         this.notificationOnChange.registerProvider(this)
-        this.guiToModel.registerObserver(Module.AUTHORS, this)
+        this.guiToModel.registerObserver(Module.BOOK_DETAILED, this)
     }
 }
