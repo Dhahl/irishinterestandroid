f1162e6da270164d42788ae624d9613ca9a59993
Balazs Perlaki-Horvath <ba.perlaki@gmail.com>
2022-01-15 15:17:32 +0100

    Fix CategoryListingFragment

--- app/src/main/java/com/irishinterest/irishinterest/categories/CategoryListingFragment.kt
+++ app/src/main/java/com/irishinterest/irishinterest/categories/CategoryListingFragment.kt
@@ -5,11 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.irishinterest.irishinterest.R
 import com.irishinterest.irishinterest.listing.CategoriesViewModel
+import com.irishinterest.irishinterest.webservice.Category
 
 class CategoryListingFragment: Fragment() {
 
