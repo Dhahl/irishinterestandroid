465f07aa1510e7bef47af4008d97af44dabdeb38
Balazs Perlaki-Horvath <ba.perlaki@gmail.com>
2022-01-15 15:08:05 +0100

    Wire up CategoryListingFragment

--- app/src/main/java/com/irishinterest/irishinterest/categories/CategoryListingFragment.kt
+++ app/src/main/java/com/irishinterest/irishinterest/categories/CategoryListingFragment.kt
@@ -5,22 +5,53 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.irishinterest.irishinterest.R
+import com.irishinterest.irishinterest.listing.CategoriesViewModel
 
 class CategoryListingFragment: Fragment() {
 
-    private lateinit var view: View
+    private lateinit var viewModel: CategoriesViewModel
+    private lateinit var category_recycler_view: RecyclerView
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        view = inflater.inflate(R.layout.categories, container, false)
+        val view = inflater.inflate(R.layout.categories, container, false)
+        category_recycler_view = view.findViewById<RecyclerView>(R.id.categoryRecyclerView)
+        category_recycler_view.layoutManager = LinearLayoutManager(this)
+
+        startLoading()
+
+        viewModel = ViewModelProvider(this).get(CategoriesViewModel::class.java)
+        viewModel.categories().observe(this, Observer(this::onData))
+
         return view
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private fun onData(categories: List<Category>?) {
+        category_recycler_view.adapter = CategoryListingAdapter(
+            categories.orEmpty(),
+            layoutInflater,
+            { category -> openCategory(category) }
+        )
+        stopLoading()
     }
+
+    private fun openCategory(category: Category) {
+        
+    }
+
+
+    private fun startLoading() {
+
+    }
+
+    private fun stopLoading() {
+
+    }
+
 }
